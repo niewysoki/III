d55d09219ed1774e4ceae985c07cf1b2e28048cb
Kacper Rzetelski <rzetelski.k@gmail.com>
2023-01-16 02:43:41 +0100

    Add static page to go server

---
+++ assets/server/static/scripts/cas.js
@@ -0,0 +1,72 @@
+/*
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License.  You may obtain a
+ * copy of the License at the following location:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+function changeLoginVis() {
+    var unobj = document.getElementById("username");
+    var unvalue = unobj.value;
+
+    var obj = document.createElement('input');
+    obj.id = "username";
+    obj.name = "username";
+    obj.className = 'field form-control';
+    obj.setAttribute('tabindex', '1');
+    obj.setAttribute('accesskey', 'i');
+
+    if (document.getElementById("fm1").loginVis.checked) {
+        obj.setAttribute('type', 'password');
+        unobj.parentNode.replaceChild(obj, unobj);
+    } else {
+        obj.setAttribute('type', 'text');
+        unobj.parentNode.replaceChild(obj, unobj);
+    }
+
+    obj.value = unvalue;
+
+    return true;
+}
+
+$(window).resize(function(){
+    $('#body').css({
+        position:'absolute',
+        left: ($(window).width() - $('#body').outerWidth())/2,
+        top: ($(window).height() - $('#body').outerHeight())/2
+    });
+});
+
+
+$(document).ready(function(){
+    $("#loginVis").click({id: "username"}, changeLoginVis);
+    //focus username field
+    $("input:visible:enabled:first").focus();
+    //flash error box
+    $('#msg.errors').animate({ backgroundColor: 'rgb(187,0,0)' }, 30).animate({ backgroundColor: 'rgb(255,238,221)' }, 500);
+
+    //flash success box
+    $('#msg.success').animate({ backgroundColor: 'rgb(51,204,0)' }, 30).animate({ backgroundColor: 'rgb(221,255,170)' }, 500);
+
+    if ( !window.console ) {
+        window.console = new Array();
+    }
+    if (!window.console || window.console == {}) {
+        window.console.log = function() {};
+    }
+    // center page
+    $(window).resize();
+});
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,74 +1,78 @@
 package server
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
-	serverassets "github.com/rzetelskik/iii/assets/server"
 	"github.com/rzetelskik/iii/pkg/store"
 	"k8s.io/klog/v2"
 	"net/http"
 )
 
 type stat struct {
-	Seen    bool `json:"seen"`
-	Clicked bool `json:"clicked"`
+	Seen     bool `json:"seen"`
+	Clicked  bool `json:"clicked"`
+	LoggedIn bool `json:"logged_in"`
 }
 
 var (
 	st store.Store[string, stat] = store.NewThreadSafeMapStore[string, stat]()
 )
 
-func handlePage(w http.ResponseWriter, r *http.Request) {
-	values := r.URL.Query()
-	if !values.Has("u") {
-		klog.V(4).Infof("received webpage request without 'u' parameter")
-		http.Error(w, "required parameter 'u' is missing", http.StatusBadRequest)
-		return
-	}
+func userMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		values := r.URL.Query()
+		if !values.Has("u") {
+			klog.V(4).Infof("received request without 'u' parameter")
+			http.Error(w, "required parameter 'u' is missing", http.StatusBadRequest)
+			return
+		}
 
-	user := values.Get("u")
-	klog.V(4).Infof("received webpage request for user %s", user)
+		user := values.Get("u")
+		next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), "user", user)))
+	})
 
-	// TODO
-	w.Header().Add("Content-Type", "text/html")
-	w.WriteHeader(http.StatusOK)
-
-	stats, ok := st.Get(user)
-	if !ok {
-		stats = stat{}
-	}
-	stats.Clicked = true
-
-	st.Put(user, stats)
 }
 
-func handlePixel(w http.ResponseWriter, r *http.Request) {
-	values := r.URL.Query()
-	if !values.Has("u") {
-		klog.V(4).Infof("received pixel request without 'u' parameter")
-		http.Error(w, "required parameter 'u' is missing", http.StatusBadRequest)
-		return
-	}
+func indexMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := r.Context().Value("user").(string)
+		klog.V(4).Infof("received webpage request for user %s", user)
 
-	user := values.Get("u")
-	klog.V(4).Infof("received pixel request for user %s", user)
+		stats, ok := st.Get(user)
+		if !ok {
+			stats = stat{}
+		}
+		stats.Clicked = true
 
-	w.Header().Add("Content-Type", "image/gif")
+		st.Put(user, stats)
 
-	_, err := w.Write(serverassets.Pixel)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+		next.ServeHTTP(w, r)
+	})
+}
 
-	stats, ok := st.Get(user)
-	if !ok {
-		stats = stat{}
-	}
-	stats.Seen = true
+func pixelMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := r.Context().Value("user").(string)
+		klog.V(4).Infof("received pixel request for user %s", user)
 
-	st.Put(user, stats)
+		stats, ok := st.Get(user)
+		if !ok {
+			stats = stat{}
+		}
+		stats.Seen = true
+
+		st.Put(user, stats)
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	// TODO
+
+	http.Redirect(w, r, "https://logowanie.uw.edu.pl/cas/login?locale=pl", http.StatusSeeOther)
 }
 
 func handleStats(w http.ResponseWriter, _ *http.Request) {
@@ -88,14 +92,25 @@ func handleStats(w http.ResponseWriter, _ *http.Request) {
 func NewServer(addr string) *http.Server {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", handlePage).
-		Methods(http.MethodGet)
+	fs := http.FileServer(http.Dir("./assets/server/static"))
 
-	r.HandleFunc("/pixel", handlePixel).
+	r.Handle("/pixel.gif", userMiddleware(pixelMiddleware(fs))).
 		Methods(http.MethodGet)
 
 	r.HandleFunc("/stats", handleStats).
 		Methods(http.MethodGet)
+
+	r.Handle("/", userMiddleware(indexMiddleware(fs))).
+		Methods(http.MethodGet)
+
+	r.HandleFunc("/", handleLogin).
+		Methods(http.MethodPost)
+
+	r.PathPrefix("/").Handler(fs)
+
+	r.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		klog.V(4).InfoS("request received", "request", r.URL.Path)
+	})
 
 	return &http.Server{
 		Addr:    addr,
